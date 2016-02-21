33fb593d34de6e4bfd83972851cf37c24c60c831
Eino Nøsterud <nosein14@student.westerdals.no>
2016-02-21 18:50:52 +0100

    bugfix

--- Js/script.js
+++ Js/script.js
@@ -4,7 +4,7 @@
         getList: [],
 
         //HTML-objekter
-        $terrorism: null,
+        $terrorisme: null,
         $mainCategories: null,
         $infoSection: null,
 
@@ -45,87 +45,76 @@
                             "cursor": "pointer"
                         })
                 });
-                
-                // Knapper før Funnet gjerningsmann START
-                
-                $(document).on("mouseenter", ".terrorism", function() {
-                 //  $("#infoSection").append($infoDiv); 
-                    SF.info(SF.infoList, 'TERRORISME' );
-                });
-                
-                 $(document).on("mouseleave", ".terrorism", function() {
-                     //Tømmer section
-                     $("#infoSection").empty();
-                });
-                
-                   
-                $(document).on("mouseenter", ".attack", function() {
-                 //  $("#infoSection").append($infoDiv); 
-                    SF.info(SF.infoList, 'ANGREP' );
-                });
-                
-                 $(document).on("mouseleave", ".attack", function() {
-                     //Tømmer section
-                     $("#infoSection").empty();
-                });
-                
-                   
-                $(document).on("mouseenter", ".ekstremist", function() {
-                 //  $("#infoSection").append($infoDiv); 
-                    SF.info(SF.infoList, 'EKSTREMIST' );
-                });
-                
-                 $(document).on("mouseleave", ".ekstremist", function() {
-                     //Tømmer section
-                     $("#infoSection").empty();
-                });
-                
-                   
-                $(document).on("mouseenter", ".radical", function() {
-                 //  $("#infoSection").append($infoDiv); 
-                    SF.info(SF.infoList, 'RADIKAL' );
-                });
-                
-                 $(document).on("mouseleave", ".radical", function() {
-                     //Tømmer section
-                     $("#infoSection").empty();
-                });
-                
-                   
-                $(document).on("mouseenter", ".terrorist", function() {
-                 //  $("#infoSection").append($infoDiv); 
-                    SF.info(SF.infoList, 'TERRORIST' );
-                });
-                
-                 $(document).on("mouseleave", ".terrorist", function() {
-                     //Tømmer section
-                     $("#infoSection").empty();
-                });
-                   
-                $(document).on("mouseenter", ".muslim", function() {
-                 //  $("#infoSection").append($infoDiv); 
-                    SF.info(SF.infoList, 'MUSLIM' );
-                });
-                
-                 $(document).on("mouseleave", ".muslim", function() {
-                     //Tømmer section
-                     $("#infoSection").empty();
-                });// Knapper før Funnet gjerningsmann END
 
-// Knapper etter funnet gjerningsmann START
+                // Knapper før Funnet gjerningsmann START
+
+                $(document).on("mouseenter", ".terrorisme", function () {
+                    SF.info(SF.infoList, 'TERRORISME');
+                });
+
+                $(document).on("mouseleave", ".terrorisme", function () {
+                    //Tømmer section
+                    $("#infoSection").empty();
+                });
+
+
+                $(document).on("mouseenter", ".angrep", function () {
+                    SF.info(SF.infoList, 'ANGREP');
+                });
+
+                $(document).on("mouseleave", ".angrep", function () {
+                    $("#infoSection").empty();
+                });
+
+
+                $(document).on("mouseenter", ".ekstremist", function () {
+                    SF.info(SF.infoList, 'EKSTREMIST');
+                });
+
+                $(document).on("mouseleave", ".ekstremist", function () {
+                    $("#infoSection").empty();
+                });
+
+
+                $(document).on("mouseenter", ".radikal", function () {
+                    SF.info(SF.infoList, 'RADIKAL');
+                });
+
+                $(document).on("mouseleave", ".radikal", function () {
+                    $("#infoSection").empty();
+                });
+
+
+                $(document).on("mouseenter", ".terrorist", function () {
+                    SF.info(SF.infoList, 'TERRORIST');
+                });
+
+                $(document).on("mouseleave", ".terrorist", function () {
+                    $("#infoSection").empty();
+                });
+
+                $(document).on("mouseenter", ".muslim", function () {
+                    SF.info(SF.infoList, 'MUSLIM');
+                });
+
+                $(document).on("mouseleave", ".muslim", function () {
+                    $("#infoSection").empty();
+                }); // Knapper før Funnet gjerningsmann END
+
+                // Knapper etter funnet gjerningsmann START
                 $(".terrorism").click(function () {
                     $(this)
                         .html(UTOYA_INFO_MODULE.getList()[6].ord)
-                    
+
                     $("#infoSection").empty();
                     SF.info(SF.infoList, 'TRAGEDIE');
 
                 });
-                
-                $(".attack").click(function () {
+
+                $(".angrep").click(function () {
                     $(this)
                         .html(UTOYA_INFO_MODULE.getList()[7].ord)
-                    
+
                     $("#infoSection").empty();
                     SF.info(SF.infoList, 'KATASTROFE');
 
@@ -133,91 +122,78 @@
                 $(".ekstremist").click(function () {
                     $(this)
                         .html(UTOYA_INFO_MODULE.getList()[8].ord)
-                    
+
                     $("#infoSection").empty();
                     SF.info(SF.infoList, 'UMENNESKELIG');
 
                 });
-                
-                $(".radical").click(function () {
+
+                $(".radikal").click(function () {
                     $(this)
                         .html(UTOYA_INFO_MODULE.getList()[9].ord)
-                    
+
                     $("#infoSection").empty();
                     SF.info(SF.infoList, 'GALSKAP');
 
                 });
-                
+
                 $(".terrorist").click(function () {
                     $(this)
                         .html(UTOYA_INFO_MODULE.getList()[10].ord)
-                    
+
                     $("#infoSection").empty();
                     SF.info(SF.infoList, 'DRAPSMANN');
 
                 });
-                
+
                 $(".muslim").click(function () {
                     $(this)
                         .html(UTOYA_INFO_MODULE.getList()[11].ord)
-                    
+
                     $("#infoSection").empty();
                     SF.info(SF.infoList, 'KRISTEN');
 
                 });
-            
-                $("#sideNote").on("mouseenter", function(){
-                   $(this)
-                        .animate({"opacity": "1"})
-                });
-                
-            
-                 $("#sideNote").on("mouseleave", function(){
-                   $(this)
-                        .animate({"opacity": "0.2"})
-                });
-                
-                
-                
 
+                $("#sideNote").on("mouseenter", function () {
+                    $(this)
+                        .animate({
+                            "opacity": "1"
+                        })
+                });
 
+                $("#sideNote").on("mouseleave", function () {
+                    $(this)
+                        .animate({
+                            "opacity": "0.2"
+                        })
+                });
 
             }(); // ----- end setEvents
 
         }, //---- end init: function
 
-
-
         info: function (list, category) {
-            
+
             $.each(list, function (i) {
-        
-             if(list[i].ord.toUpperCase().trim() == category.toUpperCase().trim()){
-                 
-                
-                var $infoDiv = $("<div>");
-                $infoDiv
-                    .html("<div>" + "<h2>" + list[i].ord + "</h2><h3>" + list[i].uttalelse + "</h3><h3>" + list[i].ord_beskrivelse + "</h3><h3>" + list[i].bruk1 + "</h3><h3>" + list[i].bruk2 + "</h3><h3>" + list[i].bruk3 + "</h3><h3>" + list[i].beskrivelse + "</h3><img src=" + list[i].tweets_src + "></img></div>")
-                    //.attr("id", i)
-                    
-                $("#infoSection").append($infoDiv);
-                
-                
+
+                if (list[i].ord.toUpperCase().trim() == category.toUpperCase().trim()) {
+
+                    var $infoDiv = $("<div>");
+                    $infoDiv
+                        .html("<div>" + "<h2>" + list[i].ord + "</h2><h3>" + list[i].uttalelse + "</h3><h3>" + list[i].ord_beskrivelse + "</h3><h3>" + list[i].bruk1 + "</h3><h3>" + list[i].bruk2 + "</h3><h3>" + list[i].bruk3 + "</h3><h3>" + list[i].beskrivelse + "</h3><img src=" + list[i].tweets_src + "></img></div>")
+                        //.attr("id", i)
+
+                    $("#infoSection").append($infoDiv);
+
                 }
-                
 
             });
 
 
         }, //end Info function
 
-        mainCatCreat: function (infoList) {
-
-
-
-        }
 
     }; //---- end SIDEFUNCTIONALITY
 
     SITEFUNCTIONALITY.init();
-    //SITEFUNCTIONALITY.info(UTOYA_INFO_MODULE.getList());
